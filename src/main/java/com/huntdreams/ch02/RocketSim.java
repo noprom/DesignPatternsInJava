8ca4726b4b0b75307d5d2f111d53db6cb657f464
noprom <tyee.noprom@qq.com>
2016-04-08 11:18:58 +0800

    add the first interface RocketSim

---
+++ src/main/java/com/huntdreams/ch02/RocketSim.java
@@ -0,0 +1,15 @@
+package com.huntdreams.ch02;
+
+/**
+ * RocketSim
+ * <p/>
+ * Author: Noprom <tyee.noprom@qq.com>
+ * Date: 4/8/16 11:17 AM.
+ */
+public interface RocketSim {
+    abstract double getMass();
+
+    public double getThrust();
+
+    void setSimTime(double t);
+}
