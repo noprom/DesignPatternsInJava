8de6468b0ff397894f508f35dd66aad128581c26
noprom <tyee.noprom@qq.com>
2016-04-08 11:32:41 +0800

    constants in interface

---
+++ src/main/java/com/huntdreams/ch02/ClassificationConstants.java
@@ -0,0 +1,12 @@
+package com.huntdreams.ch02;
+
+/**
+ * ClassificationConstants
+ * <p/>
+ * Author: Noprom <tyee.noprom@qq.com>
+ * Date: 4/8/16 11:31 AM.
+ */
+public interface ClassificationConstants {
+    static final int CONSUMER = 1;
+    static final int DISPLAY = 2;
+}
