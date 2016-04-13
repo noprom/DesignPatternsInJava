08907c4526fd5dfcf726e62da6fcec968625214d
noprom <tyee.noprom@qq.com>
2016-04-13 15:34:59 +0800

    notify the observers

--- src/main/java/java/design/patterns/ObserverPattern.java
+++ src/main/java/java/design/patterns/ObserverPattern.java
@@ -8,7 +8,58 @@ package java.design.patterns;
  * Date: 4/13/16 3:29 PM.
  */
 
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * 观察者
+ */
+class Observer {
+    public void update() {
+        System.out.println("flag value has been changed in observer");
+    }
+}
+
+interface ISubject {
+    void register(Observer observer);
+
+    void unregister(Observer observer);
+
+    void notifyObservers();
+}
+
+/**
+ * 被观察者
+ */
+class Subject implements ISubject {
+
+    List<Observer> observerList = new ArrayList<Observer>();
+    private int flag;
+
+    public int getFlag() {
+        return flag;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+        notifyObservers();
+    }
+
+    public void register(Observer observer) {
+        observerList.add(observer);
+    }
+
+    public void unregister(Observer observer) {
+        observerList.remove(observer);
+    }
+
+    public void notifyObservers() {
+        for (int i = 0, size = observerList.size();
+                i < size; i++) {
+            observerList.get(i).update();
+        }
+    }
+}
 
 public class ObserverPattern {
 
