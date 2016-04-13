31266ed187e240630f62e95e6b1a4f08dd4b6c91
noprom <tyee.noprom@qq.com>
2016-04-13 15:38:00 +0800

    rename package name

--- src/main/java/design/patterns/ObserverPattern.java
+++ src/main/java/design/patterns/ObserverPattern.java
@@ -1,4 +1,4 @@
-package java.design.patterns;
+package design.patterns;
 
 /**
  * ObserverPattern
@@ -55,7 +55,7 @@ class Subject implements ISubject {
 
     public void notifyObservers() {
         for (int i = 0, size = observerList.size();
-                i < size; i++) {
+             i < size; i++) {
             observerList.get(i).update();
         }
     }
@@ -64,6 +64,18 @@ class Subject implements ISubject {
 public class ObserverPattern {
 
     public static void main(String[] args) {
-
+        System.out.println("***Observer Pattern Demo***\n");
+        Observer o1 = new Observer();
+        Subject sub1 = new Subject();
+        sub1.register(o1);
+        System.out.println("Setting Flag = 5 ");
+        sub1.setFlag(5);
+        System.out.println("Setting Flag = 25 ");
+        sub1.setFlag(25);
+        sub1.unregister(o1);
+        // No notification this time to o1 .
+        // Since it is unregistered.
+        System.out.println("Setting Flag = 50 ");
+        sub1.setFlag(50);
     }
 }
