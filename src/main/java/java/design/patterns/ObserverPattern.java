6b84c5805ece3523b823a4c6d1a2ffb9f7df4d41
noprom <tyee.noprom@qq.com>
2016-04-13 15:30:14 +0800

    init observer pattern

---
+++ src/main/java/java/design/patterns/ObserverPattern.java
@@ -0,0 +1,18 @@
+package java.design.patterns;
+
+/**
+ * ObserverPattern
+ * 观察者模式
+ * <p/>
+ * Author: Noprom <tyee.noprom@qq.com>
+ * Date: 4/13/16 3:29 PM.
+ */
+
+
+
+public class ObserverPattern {
+
+    public static void main(String[] args) {
+
+    }
+}
