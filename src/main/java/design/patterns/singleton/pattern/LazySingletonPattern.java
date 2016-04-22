e7798f8548905fa1ac4cda4161c362198e7722c0
noprom <tyee.noprom@qq.com>
2016-04-22 10:43:52 +0800

    add double check in the synchronized method

--- src/main/java/design/patterns/singleton/pattern/LazySingletonPattern.java
+++ src/main/java/design/patterns/singleton/pattern/LazySingletonPattern.java
@@ -3,6 +3,7 @@ package design.patterns.singleton.pattern;
 /**
  * SingletonPattern
  * 单例模式:懒汉模式
+ * 在第一次调用的时候实例化自己
  * 需要使用synchronized来保持线程安全
  * <p/>
  * Author: Noprom <tyee.noprom@qq.com>
@@ -16,20 +17,37 @@ class LazyCaptain {
 
     }
 
+//    /**
+//     * 在方法上进行同步
+//     * 获得单例,但是需要使用synchronized来保持线程安全
+//     * 在性能上会有一些影响
+//     *
+//     * @return
+//     */
+//    public synchronized static LazyCaptain getCaptain() {
+//        // Lazy initialization
+//        if (captain == null) {
+//            captain = new LazyCaptain();
+//            System.out.println("New Captain selected for our team");
+//        } else {
+//            System.out.print("You already have a Captain for your team.");
+//            System.out.println("Send him for the toss.");
+//        }
+//        return captain;
+//    }
+
     /**
-     * 获得单例,但是需要使用synchronized来保持线程安全
-     * 在性能上会有一些影响
+     * 双重检查锁定
      *
      * @return
      */
-    public synchronized static LazyCaptain getCaptain() {
-        // Lazy initialization
+    public static LazyCaptain getCaptain() {
         if (captain == null) {
-            captain = new LazyCaptain();
-            System.out.println("New Captain selected for our team");
-        } else {
-            System.out.print("You already have a Captain for your team.");
-            System.out.println("Send him for the toss.");
+            synchronized (LazyCaptain.class) {
+                if (captain == null) {
+                    captain = new LazyCaptain();
+                }
+            }
         }
         return captain;
     }
