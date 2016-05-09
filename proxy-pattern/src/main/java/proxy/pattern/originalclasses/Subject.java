018ed986c7ba3ee31c3754e0ab38b5d1f49f2658
noprom <tyee.noprom@qq.com>
2016-05-09 09:00:28 +0800

    add proxy

---
+++ proxy-pattern/src/main/java/proxy/pattern/originalclasses/Subject.java
@@ -0,0 +1,11 @@
+package proxy.pattern.originalclasses;
+
+/**
+ * Subject
+ *
+ * Author: Noprom <tyee.noprom@qq.com>
+ * Date: 5/9/16 8:58 AM.
+ */
+public abstract class Subject {
+    public abstract void doSomeWord();
+}
