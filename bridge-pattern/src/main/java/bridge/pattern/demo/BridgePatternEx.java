e4e23b275f6a7bd923339a3e435bd98726188112
noprom <tyee.noprom@qq.com>
2016-05-11 05:49:12 +0800

    BRIDGE PATTERN

--- bridge-pattern/src/main/java/bridge/pattern/demo/BridgePatternEx.java
+++ bridge-pattern/src/main/java/bridge/pattern/demo/BridgePatternEx.java
@@ -1,5 +1,7 @@
 package bridge.pattern.demo;
 
+import sun.security.provider.SHA;
+
 /**
  * BridgePatternEx
  * <p/>
@@ -24,9 +26,74 @@ class GreenColor implements IColor {
     }
 }
 
+abstract class Shape {
+    // Composition
+    protected IColor color;
+
+    protected Shape(IColor c) {
+        this.color = c;
+    }
+
+    abstract void drawShape(int border);
+
+    abstract void modifyBorder(int border, int increment);
+}
+
+class Triangle extends Shape {
+
+    protected Triangle(IColor c) {
+        super(c);
+    }
+
+    @Override
+    void drawShape(int border) {
+        System.out.print("This Triangle is colored with:  ");
+        color.fillWithColor(border);
+    }
+
+    @Override
+    void modifyBorder(int border, int increment) {
+        System.out.println("\nNow we are changing the border length " + increment + " times");
+        border = border * increment;
+        drawShape(border);
+    }
+}
+
+class Rectangle extends Shape {
+    public Rectangle(IColor c) {
+        super(c);
+    }
+
+    @Override
+    void drawShape(int border) {
+        System.out.print(" This Rectangle is colored with: ");
+        color.fillWithColor(border);
+    }
+
+    @Override
+    void modifyBorder(int border, int increment) {
+        System.out.println("\n Now we are changing the border length " + increment + " times");
+        border = border * increment;
+        drawShape(border);
+    }
+}
+
 public class BridgePatternEx {
 
     public static void main(String[] args) {
-
+        System.out.println("*****BRIDGE PATTERN*****");
+        //Coloring Green to Triangle
+        System.out.println("\nColoring Triangle:");
+        IColor green = new GreenColor();
+        Shape triangleShape = new Triangle(green);
+        triangleShape.drawShape(20);
+        triangleShape.modifyBorder(20, 3);
+        //Coloring Red to Rectangle
+        System.out.println("\n\nColoring Rectangle :");
+        IColor red = new RedColor();
+        Shape rectangleShape = new Rectangle(red);
+        rectangleShape.drawShape(50);
+        //Modifying the border length twice
+        rectangleShape.modifyBorder(50, 2);
     }
 }
